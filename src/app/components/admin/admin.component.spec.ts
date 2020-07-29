75e8f05cf31c6fd08ad8c261aaf85dc8a2f0c8e2
Alexander Davidenko <alexanderdavidenko32@gmail.com>
2020-07-29 18:21:09 +0300

    admin panel tests update

--- src/app/components/admin/admin.component.spec.ts
+++ src/app/components/admin/admin.component.spec.ts
@@ -1,5 +1,6 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {RouterTestingModule} from '@angular/router/testing';
 
 import { AdminComponent } from './admin.component';
 import {ProductService} from '@app/services';
@@ -10,7 +11,7 @@ describe('AdminComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
+      imports: [HttpClientTestingModule, RouterTestingModule],
       declarations: [AdminComponent],
       providers: [ProductService]
     })
